e9836efc3c402d5d5a1c70543ed4d477be4a78ca
Yavuz Feti <yavuzfeti@outlook.com>
2025-02-17 15:58:16 +0300

    remove registrar

--- android/src/main/kotlin/com/pinciat/external_path/ExternalPathPlugin.kt
+++ android/src/main/kotlin/com/pinciat/external_path/ExternalPathPlugin.kt
@@ -1,7 +1,6 @@
 package com.pinciat.external_path
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -10,16 +9,11 @@ import io.flutter.plugin.common.MethodChannel.Result
 import android.os.Environment
 import android.content.Context
 import java.io.File
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.os.Build
 import kotlin.collections.ArrayList 
 
 /** ExternalPathPlugin */
 class ExternalPathPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
   private lateinit var context: Context
 
@@ -27,14 +21,6 @@ class ExternalPathPlugin: FlutterPlugin, MethodCallHandler {
     context = flutterPluginBinding.applicationContext
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "external_path")
     channel.setMethodCallHandler(this)
-  }
-
-  companion object {
-  @JvmStatic
-  fun registerWith(registrar: Registrar) {
-    val channel = MethodChannel(registrar.messenger(), "external_path")
-    channel.setMethodCallHandler(ExternalPathPlugin())
-    }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -45,16 +31,16 @@ class ExternalPathPlugin: FlutterPlugin, MethodCallHandler {
      }
   }
 
- fun getExternalStorageDirectories() : ArrayList<String> {
+  private fun getExternalStorageDirectories() : ArrayList<String> {
     val appsDir: Array<File> = context.getExternalFilesDirs(null)
-    var extRootPaths: ArrayList<String> = ArrayList<String>()
-    for (file: File in appsDir)
-      extRootPaths.add(file.getAbsolutePath())
-    return extRootPaths;
-      // return Environment.getExternalStorageDirectory().toString();
+    val extRootPaths = ArrayList<String>()
+    for (file: File in appsDir) {
+      extRootPaths.add(file.absolutePath)
+    }
+    return extRootPaths
   }
 
-  fun getExternalStoragePublicDirectory(type: String?) : String {
+  private fun getExternalStoragePublicDirectory(type: String?) : String {
       return Environment.getExternalStoragePublicDirectory(type).toString()
   }
 
